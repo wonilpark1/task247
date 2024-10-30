c0b8e4f278e4cc6b6634c43adb775ca2276e3a74
wonil <wonilpark1118@gmail.com>
2024-10-30 18:44:25 +0900

    프로젝트 구조 변경 리버스 프록시2

--- back/index.js
+++ back/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+
 const compression = require('compression');
 const cors = require('cors');
 const {indexRouter} = require("./src/router/indexRouter");
 const {userRouter} = require("./src/router/userRouter");
 
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
 
